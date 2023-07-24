6395d4a0f4040d27898d60eefc9c40676dbb6128
Chris Doucette <chrisdoucette15@gmail.com>
2023-07-24 10:27:11 -0400

    Updated sync description

--- cmd/sync.go
+++ cmd/sync.go
@@ -15,13 +15,8 @@ import (
 // syncCmd represents the sync command
 var syncCmd = &cobra.Command{
 	Use:   "sync",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Sync block files with Umbrella destination lists",
+	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("sync called")
 		hostname := viper.GetString("api_hostname")
